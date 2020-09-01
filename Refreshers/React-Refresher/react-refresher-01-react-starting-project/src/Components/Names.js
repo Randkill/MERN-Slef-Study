0007d78d0cb2d20a82d873ea8f6e651324de26d7
Randkill <randkill.randkill@gmail.com>
2020-09-01 20:47:24 +0430

    Moved Names component from App.js and App.css to here!

---
+++ Refreshers/React-Refresher/react-refresher-01-react-starting-project/src/Components/Names.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Names.css'
+
+const names = () => {
+    return (
+        <div>
+        <h1>My list</h1>
+    <ul className="name-list">
+      <li>Erfan</li>
+      <li>Nima</li>
+      <li>ErfanNima</li>
+    </ul>
+        </div>
+    )
+}
+
+export default names;
