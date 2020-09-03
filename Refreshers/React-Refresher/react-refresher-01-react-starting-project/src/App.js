3b649f10e698e51a072b397428833880cedb46bf
Randkill <randkill.randkill@gmail.com>
2020-09-03 16:08:05 +0430

    Now making app flexible by using React Hooksgit add -A

--- Refreshers/React-Refresher/react-refresher-01-react-starting-project/src/App.js
+++ Refreshers/React-Refresher/react-refresher-01-react-starting-project/src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css'
-import Names from './Components/Names.js'
+import Names from './Components/NamesLIst/Names.js'
+import NewName from './Components/NewName/NewName.js'
 
 const App = () => {
-  const NamesList = [
+  const [namesList, setName] = useState([
     {id: 'name1', name: 'Erfan'},
     {id: 'name2', name: 'Nima'},
     {id: 'name3', name: 'ErfaNima'}
-  ]
+  ])
+
+  const addNewNameHandler = (newName) => {
+    setName(namesList.concat(newName))
+  }
   return <div className="names">
     <h1>My list</h1>
-    <Names items={NamesList}/>
+    <NewName onAddName={addNewNameHandler}/>
+    <Names items={namesList}/>
   </div>
 };
 
