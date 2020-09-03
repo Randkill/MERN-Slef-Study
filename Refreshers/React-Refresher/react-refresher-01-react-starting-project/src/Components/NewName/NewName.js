a2eeacc9c9b6fa1357ef39f056020afbf8809687
Randkill <randkill.randkill@gmail.com>
2020-09-03 14:31:00 +0430

    Error fixed, arrow function false syntax edited!

--- Refreshers/React-Refresher/react-refresher-01-react-starting-project/src/Components/NewName/NewName.js
+++ Refreshers/React-Refresher/react-refresher-01-react-starting-project/src/Components/NewName/NewName.js
@@ -2,8 +2,19 @@ import React from 'react'
 import './NewName.css'
 
 const newName = () => {
+    addNameHandler = (event) => {
+        event.preventDefault();     //prevents frontend from sending any request to the server!
+
+        const nemName = {
+            id: Math.random().toString(),
+            name: 'NEW NAME'
+        }
+
+        console.log(newName)
+    }
+
     return(
-        <form className='name-from'>
+        <form className='name-from' onSubmit={}>
             <input type='text'></input>
             <button type="submit">Add Name</button>
         </form>
