4ee20a0a7ce1fe595b9d06cd1f78b088f796ac6b
Randkill <randkill.randkill@gmail.com>
2020-09-04 10:57:20 +0430

    Replaced setState approach with another one!

--- Refreshers/React-Refresher/react-refresher-01-react-starting-project/src/App.js
+++ Refreshers/React-Refresher/react-refresher-01-react-starting-project/src/App.js
@@ -4,14 +4,20 @@ import Names from './Components/NamesLIst/Names.js'
 import NewName from './Components/NewName/NewName.js'
 
 const App = () => {
-  const [namesList, setName] = useState([
+  //First Element: Latest State Snapshot -- Second Element: Updating Function
+  const [namesList, setName] = useState([ //useState ALLWAYS returns an array with two elements!
     {id: 'name1', name: 'Erfan'},
     {id: 'name2', name: 'Nima'},
     {id: 'name3', name: 'ErfaNima'}
   ])
 
   const addNewNameHandler = (newName) => {
-    setName(namesList.concat(newName))
+    //setName(namesList.concat(newName))    not a good appraoch if our current state, depends on the previous state.!
+    //Because, some issues might occur!!, If our new state is a new piece and not related to previous, use simple above approach!
+    //Use this approach
+    setName((prevNames) => {
+      return prevNames.concat(newName)
+    })
   }
   return <div className="names">
     <h1>My list</h1>
