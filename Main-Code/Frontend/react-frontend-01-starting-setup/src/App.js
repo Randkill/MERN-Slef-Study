30cd6e9d088b9bd4d5247084e131fbff0ff42d8f
Randkill <randkill.randkill@gmail.com>
2020-09-06 16:37:26 +0430

    Adding REDIRECT and SWITCH from REACT-ROUTER-DOM + NewPlace simple component!

--- Main-Code/Frontend/react-frontend-01-starting-setup/src/App.js
+++ Main-Code/Frontend/react-frontend-01-starting-setup/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import User from './user/pages/User.js'
+import NewPlace from './places/pages/NewPlace.js'
 
 const App = () => {
   return(
     <Router>
-      <Route path="/user">
+      <Switch>
+      <Route path="/" exact={true}>
         <User/>
       </Route>
+      <Route path="/places/new" exact={true}>
+        <NewPlace/>
+      </Route>
+      <Redirect to="/"/>
+      </Switch>
     </Router>
   )
 }
