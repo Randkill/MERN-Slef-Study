d87ad577669d06c51fa842c749681338a74d9032
Randkill <randkill.randkill@gmail.com>
2020-09-06 17:12:27 +0430

    Users page initialized and tested with a dummy value!

--- Main-Code/Frontend/react-frontend-01-starting-setup/src/user/pages/User.js
+++ Main-Code/Frontend/react-frontend-01-starting-setup/src/user/pages/User.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
+import UsersList from '../components/UsersList.js'
+
 const user = () => {
+    const USERS = [{
+        id: 'u1',
+        name: 'Randkill',
+        image: 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fappsource.microsoft.com%2Fen-us%2Fproduct%2Fpower-bi-visuals%2FWA104381835%3Ftab%3DOverview&psig=AOvVaw15RAPhwcS6RHHXHVT6t7Fu&ust=1599482404167000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCMCFuLbG1OsCFQAAAAAdAAAAABAD',
+        places: 3
+
+    }]
+
     return(
         <div>
-            <h1>User's Page</h1>
+            <UsersList items={USERS}/>
         </div>
     )
 }
