a2b4e3cd15dd120bf2e99ffa9392ad4c9972940a
Randkill <randkill.randkill@gmail.com>
2020-09-09 17:16:45 +0430

    Now using MainNavigation.js in App.js!

--- Main-Code/Frontend/react-frontend-01-starting-setup/src/App.js
+++ Main-Code/Frontend/react-frontend-01-starting-setup/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
+
 import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 import User from './user/pages/User.js'
 import NewPlace from './places/pages/NewPlace.js'
+import MainHeaderNavigation from './shared/components/Navigation/MainNavigation.js'
 
 const App = () => {
   return(
     <Router>
-      <Switch>
-      <Route path="/" exact={true}>
-        <User/>
-      </Route>
-      <Route path="/places/new" exact={true}>
-        <NewPlace/>
-      </Route>
-      <Redirect to="/"/>
-      </Switch>
+      <MainHeaderNavigation />
+      <main>
+        <Switch>
+        <Route path="/" exact={true}>
+          <User/>
+        </Route>
+        <Route path="/places/new" exact={true}>
+          <NewPlace/>
+        </Route>
+        <Redirect to="/"/>
+        </Switch>
+      </main>
     </Router>
   )
 }
