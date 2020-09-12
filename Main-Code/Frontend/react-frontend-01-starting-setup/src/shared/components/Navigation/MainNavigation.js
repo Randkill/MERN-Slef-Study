935ef6d097121de9194a3f7e91e720423fd66626
Randkill <randkill.randkill@gmail.com>
2020-09-12 17:05:10 +0430

    Now adding NavLinks to the MainNavigation.js component!

--- Main-Code/Frontend/react-frontend-01-starting-setup/src/shared/components/Navigation/MainNavigation.js
+++ Main-Code/Frontend/react-frontend-01-starting-setup/src/shared/components/Navigation/MainNavigation.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './MainNavigation.css';
 import MainHeader from './MainHeader.js'
 import { Link } from 'react-router-dom'
+import NavLinks from './NavLinks.js';
 
 const MainNavigation = (props) => {
     return(
@@ -16,7 +17,7 @@ const MainNavigation = (props) => {
                 <Link to="/">Your Places</Link>
             </h1>
             <nav>
-                ...
+                <NavLinks/>
             </nav>
         </MainHeader>
     )
