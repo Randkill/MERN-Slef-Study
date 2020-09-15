b13e8446314ac601a7291b0ca6ce75fb913707ee
Randkill <randkill.randkill@gmail.com>
2020-09-15 11:06:56 +0430

    Using UserPlaces page!

--- Main-Code/Frontend/react-frontend-01-starting-setup/src/App.js
+++ Main-Code/Frontend/react-frontend-01-starting-setup/src/App.js
@@ -1,27 +1,36 @@
-import React from 'react';
+import React from "react";
 
-import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
-import User from './user/pages/User.js'
-import NewPlace from './places/pages/NewPlace.js'
-import MainHeaderNavigation from './shared/components/Navigation/MainNavigation.js'
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import User from "./user/pages/User.js";
+import NewPlace from "./places/pages/NewPlace.js";
+import MainHeaderNavigation from "./shared/components/Navigation/MainNavigation.js";
+import UserPlaces from "./places/pages/UserPlaces.js";
 
 const App = () => {
-  return(
+  return (
     <Router>
       <MainHeaderNavigation />
       <main>
         <Switch>
-        <Route path="/" exact={true}>
-          <User/>
-        </Route>
-        <Route path="/places/new" exact={true}>
-          <NewPlace/>
-        </Route>
-        <Redirect to="/"/>
+          <Route path="/" exact={true}>
+            <User />
+          </Route>
+          <Route path="/:userId/places" exact={true}>
+            <UserPlaces />
+          </Route>
+          <Route path="/places/new" exact={true}>
+            <NewPlace />
+          </Route>
+          <Redirect to="/" />
         </Switch>
       </main>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
