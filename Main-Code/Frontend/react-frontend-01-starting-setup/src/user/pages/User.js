e755c08b9300dd82e5f588122513b02a866609f8
Randkill <randkill.randkill@gmail.com>
2020-09-30 11:18:36 +0330

    Not changed!

--- Main-Code/Frontend/react-frontend-01-starting-setup/src/user/pages/User.js
+++ Main-Code/Frontend/react-frontend-01-starting-setup/src/user/pages/User.js
@@ -7,7 +7,7 @@ const user = () => {
         id: 'u1',
         name: 'Randkill',
         image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Keanu_Reeves_2014.jpg/170px-Keanu_Reeves_2014.jpg',
-        places: 3
+        places: 3,
     }]
     return(
         <div>
